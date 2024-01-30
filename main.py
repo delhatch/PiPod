50abf6a8c1a0fb54c5dcfdc81c55e3e871eb50c2
Del Hatch <engineering_type@yahoo.com>
2024-01-30 10:55:09 -0800

    Initial: copied from delhatch/PiPod_Zero2W

---
+++ main.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+import playback
+import display
+import navigation
+import device
+import pygame
+import RPi.GPIO as GPIO
+
+done = False
+music = playback.music()
+music.enableEQ()
+view = display.view()
+menu = navigation.menu()
+PiPod = device.PiPod()
+
+# Updating 6700 files takes 50 seconds
+#print("Starting update.")
+#music.updateLibrary()  # This creates the info.csv file by reading every .MP3 file metadata.
+#print("Done with update")
+menu.loadMetadata()   # This reads the info.csv file
+status = PiPod.getStatus()
+songMetadata = music.getStatus()
+
+# If you want to do a thing every periodically (ePaper?), check for this event in the pygame que.
+displayUpdate = pygame.USEREVENT + 1
+pygame.time.set_timer(displayUpdate, 200)
+
+view.update(status, menu.menuDict, songMetadata)
+
+while not done:
+    music.loop()    # Checks if song has ended, and starts playing next song on que (if not empty).
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                PiPod.toggleSleep()
+
+            elif event.key == pygame.K_u:
+                music.volumeUp()
+
+            elif event.key == pygame.K_d:
+                music.volumeDown()
+
+            elif event.key == pygame.K_UP:
+                if status[2]:
+                    music.volumeUp()
+                elif menu.menuDict["current"] == "musicController":
+                    menu.gotomenu()
+                else:
+                    action = menu.up()
+
+            elif event.key == pygame.K_DOWN:
+                if status[2]:
+                    music.volumeDown()
+                elif menu.menuDict["current"] == "musicController":
+                    music.shuffle()
+                    menu.menuDict["Queue"] = music.playlist
+                else:
+                    action = menu.down()
+
+            elif event.key == pygame.K_LEFT:
+                if status[2] or menu.menuDict["current"] == "musicController":
+                    music.prev()
+                else:
+                    action = menu.left()
+
+            elif event.key == pygame.K_RIGHT:
+                if status[2] or menu.menuDict["current"] == "musicController":
+                    music.next()
+                else:
+                    action = menu.right()
+                    if action == "updateList":
+                        music.updateList(menu.menuDict["Queue"])
+
+            elif event.key == pygame.K_RETURN:
+                if status[2] or menu.menuDict["current"] == "musicController":
+                    music.playPause()
+                else:
+                    action = menu.select()
+                    if action == "play":
+                        music.loadList(menu.menuDict["Queue"])
+                        music.play()
+                    elif action == "clearQueue":
+                        menu.menuDict["Queue"] = []
+                        music.clearQueue()
+                    elif action == "updateLibrary":
+                        if music.updateLibrary():
+                            done = True
+                            pass
+                    elif action == "toggleSleep":
+                        PiPod.toggleSleep()
+                    elif action == "shutdown":
+                        while not PiPod.shutdown():
+                            view.popUp("Shutdown")
+                    elif action == "reboot":
+                        while not PiPod.reboot():
+                            view.popUp("Reboot")
+                    elif action == "playAtIndex":
+                        if menu.menuDict["selectedItem"] == 0:
+                            music.clearQueue()
+                            menu.menuDict["Queue"] = []
+                        else:
+                            music.playAtIndex(menu.menuDict["selectedItem"]-1)
+                    elif action == "EQOn":
+                        music.enableEQ()
+                    elif action == "EQOff":
+                        music.disableEQ()
+
+        if event.type  == displayUpdate:
+            status = PiPod.getStatus()         # Reads battery voltage, gets "status[2]" = backlight on/off
+            songMetadata = music.getStatus()   # Get song length, how far in, song info, vol, playlist
+            view.update(status, menu.menuDict, songMetadata) # Writes to frame buffer
+            view.refresh()
+        # The next line gets executed every time we check for an event on the que, no matter the event.
+        pass
+    pygame.time.Clock().tick(30)  # Limit the framerate to 20 FPS, to retain CPU resources
+
---
+++ playback.py
@@ -0,0 +1,231 @@
+import vlc
+import random
+import alsaaudio
+import os
+import csv
+import taglib
+
+MY_EQ = [10,8,3,0,0,0,0,0,7,10]
+eqFreqs = []
+
+class music():
+    UseMeta = False  # If False, use MP3 filename as the source of title/artist metadata.
+                     # If True,  use the metadata inside the MP3 file.
+    volume = 60
+    playlist = [["", "", "", ""]]
+    currentSongIndex = 0
+    flagEQ = False
+    AlbumNoKey = 0
+    AlbumEmptyField = 0
+    ArtistNoKey = 0
+    ArtistEmptyField = 0
+    TitleNoKey = 0
+    TitleEmptyField = 0
+
+    def __init__(self):
+        self.vlcInstance = vlc.Instance('--no-video')
+        self.player = self.vlcInstance.media_player_new()
+        self.alsa = alsaaudio.Mixer(alsaaudio.mixers()[0])
+        self.alsa.setvolume(self.volume)
+        # Setup Audio Equalizer
+        self.eq = vlc.AudioEqualizer()
+        bandCount = vlc.libvlc_audio_equalizer_get_band_count()  # Returns: 10
+        eqAmps = MY_EQ
+        for bandIndex in range(bandCount):
+            amp = eqAmps[bandIndex]
+            self.eq.set_amp_at_index(amp, bandIndex)
+            eqFreqs.append(vlc.libvlc_audio_equalizer_get_band_frequency(bandIndex))
+        print(f'Freq: {" ".join(map(str, eqFreqs))}')
+        print(f'Amp: {" ".join(map(str, eqAmps))}')
+
+    def enableEQ(self):
+        if self.flagEQ:
+            pass   # Do nothing if EQ already enabled.
+        else:
+            self.flagEQ = True
+            self.player.set_equalizer(self.eq)
+        return 1
+
+    def disableEQ(self):
+        if self.flagEQ:
+            self.flagEQ = False
+            self.player.set_equalizer(None)
+        else:
+            pass   # Do nothing if EQ already disabled.
+        return 1
+
+    def getStatus(self):
+        status = {
+            "songLength": self.player.get_length(),
+            "currentTime": self.player.get_time(),
+            "currentSong": self.playlist[self.currentSongIndex],
+            "volume": self.alsa.getvolume()[0],
+            "playlist": self.playlist
+        }
+        return status
+
+    def loop(self):
+        if self.player.get_state() == vlc.State.Ended and self.currentSongIndex < len(self.playlist)-1:
+            self.currentSongIndex += 1
+            self.play()
+
+    def loadList(self, songList):
+        self.playlist = songList
+        self.currentSongIndex = 0
+        self.play()
+
+    def updateList(self, newList):
+        if self.playlist[0] == ["", "", "", ""]:
+            self.playlist.pop(0)
+            self.playlist = newList
+            self.currentSongIndex = 0
+            self.play()
+        else:
+            self.currentSongIndex = newList.index(self.playlist[self.currentSongIndex])
+            self.playlist = newList
+
+    def play(self):
+        #print(currentSong)
+        self.player.set_media(self.vlcInstance.media_new_path(self.playlist[self.currentSongIndex][0]))
+        print("About to play.")
+        self.player.play()
+
+    def playAtIndex(self, index):
+        self.currentSongIndex = index
+        self.player.set_media(self.vlcInstance.media_new_path(self.playlist[self.currentSongIndex][0]))
+        self.player.play()
+
+    def playPause(self):
+        if self.player.get_state() == vlc.State.Playing:
+            self.player.pause()
+        elif not self.player.get_state() == vlc.State.Playing:
+            self.player.play()
+
+    def shuffle(self):
+        # Before shuffling remove the already played songs to make sure these don't get played again
+        tempPlaylist = self.playlist[self.currentSongIndex + 1::]
+        random.shuffle(tempPlaylist)
+        # Add the already played songs to the front again
+        self.playlist = self.playlist[:self.currentSongIndex + 1] + tempPlaylist
+
+    def clearQueue(self):
+        self.playlist = [["", "", "", ""]]
+        self.currentSongIndex = 0
+        self.player.stop()
+
+    def next(self):
+        if self.currentSongIndex < len(self.playlist)-1:
+            self.currentSongIndex += 1
+            self.play()
+
+    def prev(self):
+        if self.currentSongIndex > 0:
+            self.currentSongIndex -= 1
+            self.play()
+
+    def volumeUp(self):
+        if self.volume <= 95:
+            self.volume += 5
+            self.alsa.setvolume(self.volume)
+            print(self.volume)
+
+    def volumeDown(self):
+        if self.volume > 5:
+            self.volume -= 5
+            self.alsa.setvolume(self.volume)
+
+    def updateLibrary(self):
+        self.playPause()
+        fileList = []
+        print("Updating metadata")
+        #musicPath = "/home/pi/musicPlayer/Music/"
+        musicPath = "/home/drh/Music/"
+
+        for path, dirs, files in os.walk(musicPath):
+            for file in files:
+                if file.endswith('.mp3') or file.endswith('.MP3') or file.endswith('.Mp3') or file.endswith('.m4a') or file.endswith('.wav') or file.endswith('.wma'):
+                    fileList.append(os.path.join(path, file))
+                    #print(os.path.join(path, file))
+
+        file = open("info.csv", "w", newline="")
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for i in fileList:
+            audiofile = taglib.File(i)
+            song = audiofile.tags
+            if self.UseMeta:  # Metadata source = metadata in the MP3 file.
+                # Check to see if the "ARTIST" field is empty, or does not exist.
+                #    If so, fill it in with "Not Sure"
+                if( 'ARTIST' in song ):
+                    if( song['ARTIST'] == [] ):  # Key exists, but points to empty field.
+                        self.ArtistEmptyField += 1
+                        song['ARTIST'] = ['NOT SURE ARTIST']  # Found none of these
+                    else:
+                        pass  # Artist field was filled in. Is legit.
+                else:
+                    self.ArtistNoKey += 1
+                    song['ARTIST'] = ['Not Sure ARTIST']   # Found 26 of these
+
+                # Check to see if the "TITLE" field is empty, or does not exist.
+                #    If so, fill it in with "Not Sure"
+                if( 'TITLE' in song ):
+                    if( song['TITLE'] == [] ):  # Key exists, but points to empty field.
+                        self.TitleEmptyField += 1
+                        song['TITLE'] = ['NOT SURE TITLE']  # Found none of these
+                    else:
+                        pass  # Album field was filled in. Is legit.
+                else:
+                    self.TitleNoKey += 1
+                    song['TITLE'] = ['Not Sure TITLE']   # Found 41 of these
+
+            else:   # Metadata source = the MP3 filename string.
+                # MP3 filenames must look exactly like this:
+                #    "/home/path/user/Thisartist - Thistitle.mp3"
+                artist = i.split(" -")
+                newartist = artist[0].split("/")
+                stringArtist = newartist[4].lstrip()
+                song['ARTIST'] = [stringArtist]
+                try:
+                    title = i.split("- ")
+                except:
+                    print("Error splitting!",i)
+                try:
+                    newtitle = title[1].split(".")
+                except:
+                    print("Error splitting",i)
+                stringTitle = newtitle[0].lstrip()
+                song['TITLE'] = [stringTitle]
+
+            # Check to see if the "ALBUM" field is empty, or does not exist.
+            #    If so, fill it in with "Not Sure"
+            if( 'ALBUM' in song ):
+                if( song['ALBUM'] == [] ):  # Key exists, but points to empty field.
+                    self.AlbumEmptyField += 1
+                    song['ALBUM'] = ['NOT SURE ALBUM']  # Found 1635 of these
+                else:
+                    pass  # Album field was filled in. Is legit.
+            else:
+                self.AlbumNoKey += 1
+                song['ALBUM'] = ['Not Sure ALBUM']   # Found 174 of these
+
+            # At this point, the following writer call should never fail.
+            try:
+                writer.writerow((i, song["ARTIST"][0], song["ALBUM"][0], song["TITLE"][0]))
+            except:
+                print("Unknown write error",i)
+                try:
+                    print(song["ARTIST"])
+                    print(song["ALBUM"])
+                    print(song["TITLE"])
+                except:
+                    pass
+        file.close()
+        print("Done writing metadata file.")
+        print("AlbumNoKey = ", self.AlbumNoKey)
+        print("AlbumEmptyField = ", self.AlbumEmptyField)
+        if self.UseMeta:
+            print("ArtistNoKey = ", self.ArtistNoKey)
+            print("ArtistEmptyField = ", self.ArtistEmptyField)
+            print("TitleNoKey = ", self.TitleNoKey)
+            print("TitleEmptyField = ", self.TitleEmptyField)
+        return 1
