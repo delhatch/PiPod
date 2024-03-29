7b04a21ef15ea64f1922806c850362337e221cc1
Del Hatch <engineering_type@yahoo.com>
2024-03-29 16:45:03 -0700

    Delete global.py

--- global.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-p = subprocess.run( ["python3", "/home/drh/PiPod_Zero2W/Sofware/main.py"] )
-#print( p )
-if p.returncode == 1:
-  print("FAILED. Re-launching...")
-  p = subprocess.run( ["python3", "/home/drh/PiPod_Zero2W/Sofware/main.py"] )
-
